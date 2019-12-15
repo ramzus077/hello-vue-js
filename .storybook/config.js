6c0b8b4cbb5791fb43fc9e9a6772b4e41a63973a
Ramzi Arif <arif.ramzi.1990@gmail.com>
2019-12-15 01:52:55 -0500

    add JEST

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,3 +2,10 @@ import { configure } from '@storybook/vue';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
+/*
+const req = require.context('../stories', true, /.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);*/
--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+module.exports = api => ({
+  presets: ['@vue/app'],
+  ...(api.env('test') && { plugins: ['require-context-hook'] }),
+});
