cb26f1aa2d36bebddbf1c9f601553a47ea418982
Ramzi Arif <arif.ramzi.1990@gmail.com>
2019-12-15 03:50:08 -0500

    addons knobs

--- stories/2-Task.stories.js
+++ stories/2-Task.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
-
+import { withKnobs, object } from "@storybook/addon-knobs";
 import Task from '../src/components/Task.vue';
 
 export const task = {
@@ -15,28 +15,44 @@ export const methods = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-storiesOf('Task', module)
-  .add('default', () => {
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
+
+storiesOf("Task", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
     return {
       components: { Task },
       template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
-      data: () => ({ task }),
-      methods,
+      props: {
+        task: {
+          type: Object,
+          default: object("task", { ...task })
+        }
+      },
+      methods
     };
   })
-  .add('pinned', () => {
+  .add("pinned", () => {
     return {
       components: { Task },
       template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
-      data: () => ({ task: { ...task, state: 'TASK_PINNED' } }),
-      methods,
+      data: () => ({ task: { ...task, state: "TASK_PINNED" } }),
+      methods
     };
   })
-  .add('archived', () => {
+  .add("archived", () => {
     return {
       components: { Task },
       template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
-      data: () => ({ task: { ...task, state: 'TASK_ARCHIVED' } }),
-      methods,
+      data: () => ({ task: { ...task, state: "TASK_ARCHIVED" } }),
+      methods
+    };
+  })
+  .add("longTitle", () => {
+    return {
+      components: { Task },
+      template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
+      data: () => ({ task: { ...task, title: longTitle } }),
+      methods
     };
   });
