4241c411e1b9e5aca343a1ab5cde54bf696167d3
Ramzi Arif <arif.ramzi.1990@gmail.com>
2019-12-15 03:06:04 -0500

    add dark mode

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,15 @@
-import { configure } from '@storybook/vue';
+import { configure,addParameters } from '@storybook/vue';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
+addParameters({
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: 'black' },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'red' }
+  }
+});
 /*
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
